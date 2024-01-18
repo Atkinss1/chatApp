f3c9f4b67931ef570661c3ae15a27f57a0127383
Atkinss1 <jordanatkins@live.com>
2024-01-18 16:51:26 -0700

    refactor createServer to Express

--- server/index.js
+++ server/index.js
@@ -1,9 +1,22 @@
-import { createServer } from 'http';
+import express from 'express';
+import path from 'path';
 import { Server } from 'socket.io';
+import { fileURLToPath } from 'url';
 
-const httpServer = createServer();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-const io = new Server(httpServer, {
+const PORT = process.env.PORT || 3500;
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+const expressServer = app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
+
+const io = new Server(expressServer, {
   cors: {
     origin: process.env.NODE_ENV === 'production'? false : 
     ['http://localhost:5500']
@@ -20,8 +33,4 @@ io.on('connection', socket => {
     console.log(`${data}`);
     io.emit('message', `${socket.id.substring(0,5)}: ${data}`);
   })
-})
-
-httpServer.listen(3500, () => {
-  console.log(`listening on port 3500`);
 })
